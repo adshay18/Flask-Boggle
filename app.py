44041e64bc233861b78bb33f4242212fbf86fdaa
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-01-06 16:10:27 -0500

    initialize app route and get boggle board to appear with Jinja

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 from boggle import Boggle
+from flask import Flask, render_template
+from flask_debugtoolbar import DebugToolbarExtension
 
 boggle_game = Boggle()
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "boggleme"
+debug = DebugToolbarExtension(app)
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+@app.route('/')
+def start_game():
+    board = boggle_game.make_board()
+    return render_template('index.html', board=board)
