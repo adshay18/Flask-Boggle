85bab3cbcc0d91891324c54df55152711890348f
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-01-07 18:15:08 -0500

    check for valid word and respond with message

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from boggle import Boggle
-from flask import Flask, render_template, session
+from flask import Flask, render_template, session, request, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
@@ -11,7 +11,18 @@ boggle_game = Boggle()
 
 @app.route('/')
 def start_game():
+    '''Method to display index.html and start the game'''
     board = boggle_game.make_board()
     session["game_board"] = board
     saved_board = session["game_board"]
     return render_template('index.html', board=saved_board)
+
+@app.route('/validate-word')
+def test_word():
+    '''Check if the word is in the dictionary'''
+    
+    word = request.args["word"]
+    game_board = session["game_board"]
+    response = boggle_game.check_valid_word(game_board, word)
+
+    return jsonify({'result': response})
--- static/app.js
+++ static/app.js
@@ -1,7 +1,23 @@
 form = document.getElementById('form');
-guess = document.getElementById('guess');
+$guess = $('#guess');
 
-form.addEventListener('submit', function(e) {
+function displayMessage(msg) {
+	$('.msg').text(msg);
+}
+
+form.addEventListener('submit', async function(e) {
 	e.preventDefault();
+	let word = $guess.val();
+	const res = await axios.get('/validate-word', { params: { word: word } });
+
+	if (res.data.result === 'not-word') {
+		displayMessage(`'${word}' is not a real word.`);
+	}
+	if (res.data.result === 'not-on-board') {
+		displayMessage(`'${word}' is not on this board.`);
+	}
+	if (res.data.result === 'ok') {
+		displayMessage(`'${word}' was accepted.`);
+	}
 	guess.value = '';
 });
